02b47808a60d74702f0522d8a89a84bb9c8ea8d3
HaiAu2501 <kiet.nvt220032@sis.hust.edu.vn>
2025-08-02 12:19:27 +0700

    aa

--- tsp_gls/eval.py
+++ tsp_gls/eval.py
@@ -5,12 +5,12 @@ from scipy.spatial import distance_matrix
 from concurrent.futures import ThreadPoolExecutor
 from gls import run_tsp_gls
 
-OPTIMAL = {
-    "50": 5.68457994395107,
-    "100": 7.778580370400294,
-    "200": 10.71194600194464,
-    "500": 16.499886342078646
-}
+# OPTIMAL = {
+#     "50": 5.68457994395107,
+#     "100": 7.778580370400294,
+#     "200": 10.71194600194464,
+#     "500": 16.499886342078646
+# }
 
 def solve_instance(coordinates: np.ndarray, perturbation_moves: int, iter_limit: int, seed: int) -> float:
     """
@@ -84,13 +84,13 @@ def process_file(path: str, perturbation_moves: int, iter_limit: int) -> np.ndar
 
 perturbation_moves_map = {
     50: 30,
-    100: 40,
+    100: 20,
     200: 40,
     500: 50,
 }
 iter_limit_map = {
     50: 1000,
-    100: 2000,
+    100: 500,
     200: 2000,
     500: 1000,
 }
@@ -101,14 +101,14 @@ def run(size):
     path = os.path.join(current_dir, 'dataset', f'test_TSP{size}.npy')
     
     # Process dataset
-    results = process_file(path, perturbation_moves_map[size], iter_limit_map[size])
+    results = process_file(path, perturbation_moves=10, iter_limit=10)
 
     # Add to total cost
     print(results.mean())
     # Calculate optimality gap
-    if str(size) in OPTIMAL:
-        opt_gap = (results.mean() - OPTIMAL[str(size)]) / OPTIMAL[str(size)] * 100
-        print(f"Optimality gap: {opt_gap:.6f}%")
+    # if str(size) in OPTIMAL:
+    #     opt_gap = (results.mean() - OPTIMAL[str(size)]) / OPTIMAL[str(size)] * 100
+    #     print(f"Optimality gap: {opt_gap:.6f}%")
 
 if __name__ == "__main__":
     # Get mode from command line argument
